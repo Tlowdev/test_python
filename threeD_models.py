7f4ee3341b29b2338379ccf5438efd3e59c5143a
Tylor Roth <60242401+Tlowdev@users.noreply.github.com>
2022-10-01 14:13:01 -0700

    changed comment tags in function

--- threeD_models.py
+++ threeD_models.py
@@ -1,15 +1,15 @@
 
 
 def print_models(unprinted_designs, printed_designs):
-#Simulate printing design, until none are left.
-#Move each design into printed_designs after printing.
+"""Simulate printing design, until none are left."""
+"""Move each design into printed_designs after printing."""
     while unprinted_designs:
         current_design = unprinted_designs.pop()
         print(f'Printing: {current_design}')
         printed_designs.append(current_design)
 
 def completed_models(printed_designs):
-#display printed_designs
+"""display printed_designs"""
     print('\nThe following models have been printed:')
     for completed in printed_designs:
         print(completed)
