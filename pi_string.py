a781d9edfb19be624d5643ab1b13449116510c60
Tylor Roth <60242401+Tlowdev@users.noreply.github.com>
2022-10-07 23:32:11 -0700

    Minor changes: type check for clarity

--- pi_string.py
+++ pi_string.py
@@ -5,18 +5,20 @@ with open(file_name) as file_object:
     lines = file_object.readlines()
 
 #creating empty string to pass the file to
-pi_string = ''
+pi_string: str = ''
 for line in lines:
     pi_string += line.rstrip()
 
 #created a variable with my birthday (mmddyyyy)
-my_b_day = '04281990'
+my_b_day: str = '04281990'
 
-def birthday(my_b_day):
-    if my_b_day in pi_string:
-      print('My birthday appears in the 1st million digits of pi!')
-    else:
-      print('My birthday is not in pi.')
+def birthday(my_b_day: str) -> str:
+  """Passing in argument to check if it exists within the string."""
+  if my_b_day in pi_string:
+    print('My birthday appears in the 1st million digits of pi!')
+  else:
+    print('My birthday is not in pi.')
+  return my_b_day
     
 
 
