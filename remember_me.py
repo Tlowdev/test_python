78167856283e401dc81c680741bd89502137cbab
Tylor Roth <60242401+Tlowdev@users.noreply.github.com>
2022-10-10 21:37:58 -0700

    this file is buggy and needs to be reviewed

---
+++ remember_me.py
@@ -0,0 +1,35 @@
+import json
+#Load the username, if it has been stored previously.
+#otherwise, prompt for username and store it.
+def get_stored_username():
+    """Get stored username if available"""
+    file_name = 'username.json'   
+    try:
+        with open(file_name) as f:
+          username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for new username"""
+    username = input("what is your username? ")
+    file_name = 'username.json'
+    with open(file_name, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet user by name"""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}")
+    else:
+       username = get_new_username()
+       print(f"We will remember you when you come back, {username}!")    
+greet_user()
+
+
+    
+
